a81f12989a7d56bc2f2b45ae995b730b8d984e26
M H <wooteacher.main@gmail.com>
2026-07-10 12:36:59 +0900

    fix: remove unused proxy

--- proxy.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-export function proxy(_request: NextRequest) {
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/admin/:path*"],
-};
